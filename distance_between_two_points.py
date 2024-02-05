3f3d5dc7ed03d4aadff17f9c27db84b757fe29da
Andrew Alsing <andrew32alsing@gmail.com>
2024-02-05 11:40:27 -0800

    completed exercise 5

--- distance_between_two_points.py
+++ distance_between_two_points.py
@@ -2,20 +2,27 @@
 # Example for Compose Methods: Extract Method.
 
 import math
-xc1 = 4
-yc1 = 4.25
 
-xc2 = 53
-yc2 = -5.35
-# Calculate the distance between the two circle
-distance = math.sqrt((xc1-xc2)**2 + (yc1 - yc2)**2)
-print('distance', distance)
-# *** somewhere else in your program ***
-xa = -36
-ya = 97
+def calculate_distance_between_two_circles(xc1, yc1, xc2, yc2):
+    return math.sqrt((xc1-xc2)**2 + (yc1 - yc2)**2)
 
-xb = .34
-yb = .91
-# calcualte the length of vector AB vector which is a vector between A and B points.
-length = math.sqrt((xa-xb)*(xa-xb) + (ya-yb)*(ya-yb))
-print('length', length)
+def calculate_vector_length(xa, ya, xb, yb):
+    return math.sqrt((xa-xb)*(xa-xb) + (ya-yb)*(ya-yb))
+
+def main():
+    xc1 = 4
+    yc1 = 4.25
+
+    xc2 = 53
+    yc2 = -5.35
+
+    xa = -36
+    ya = 97
+
+    xb = .34
+    yb = .91
+
+    print('distance between two circles:', calculate_distance_between_two_circles(xc1, yc1, xc2, yc2))
+    print('Lengh of vector:', calculate_vector_length(xa, ya, xb, yb))
+
+main()
