f2392363fe78e789b7baea875657a6034332d881
Andrew Alsing <andrew32alsing@gmail.com>
2024-02-05 11:24:52 -0800

    completed exercise 4

--- introduce_explaining_variable3.py
+++ introduce_explaining_variable3.py
@@ -5,8 +5,22 @@ MEDIUM = 600000
 COOKED_CONSTANT = 0.05
 
 def is_cookeding_criteria_satisfied(time, temperature, pressure, desired_state):
-    if desired_state == 'well-done' and time * temperature * pressure * COOKED_CONSTANT >= WELL_DONE: 
-        return True
-    if desired_state == 'medium' and time * temperature * pressure * COOKED_CONSTANT >= MEDIUM:
-        return True
-    return False
+    cooking_value = time * temperature * pressure * COOKED_CONSTANT
+    
+    is_well_done = desired_state == 'well-done' and cooking_value >= WELL_DONE
+    is_medium = desired_state == 'medium' and cooking_value >= MEDIUM
+    
+    return is_well_done or is_medium
+
+def main():
+    time = 30
+    temp = 103
+    pressure = 20
+    desired_state = 'well-done'
+    
+    if is_cookeding_criteria_satisfied(time, temp, pressure, desired_state):
+        print('cooking is done.')
+    else:
+        print('ongoing cooking.')
+
+main()
